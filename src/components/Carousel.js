06ac83b869f6c57f04a8bb8db454aeb13cb77c18
Kim Hobin <isb0573@naver.com>
2023-10-22 20:06:13 +0900

    carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Carousel = (props) => {
-  const [currentIdx, setCurrentIdx] = useState(1);
+  const [currentIdx, setCurrentIdx] = useState(0);
 
   const testImgs = [
     { src: "https://img.freepik.com/premium-vector/modern-passionate-badminton-player-in-action-logo_293731-95.jpg" },
     { src: "https://png.pngtree.com/element_pic/17/03/03/b8ef40a996638082467a3e5a2e8c19de.jpg" },
     { src: "https://img.hani.co.kr/imgdb/resize/2017/0420/00500016_20170420.JPG" },
   ]
+
+  // useEffect(() => {
+  //   console.log("currentIdx");
+  // }, [currentIdx]);
+
+  if (currentIdx === 3) {
+    setCurrentIdx(0);
+  }
 
   return (
     <div style={{ display: "flex", justifyContent: "center", }}>
@@ -18,7 +26,7 @@ const Carousel = (props) => {
       {/* <img src={testImgs[currentIdx].src} width="200px" height="200px" ></img> */}
       <div style={{ display: "flex", width: "200px", height: "200px", overflow: "hidden", }}>
         <div style={{ display: "flex", objectFit: "contain", transform: `translateX(${-1 * (100 * currentIdx)}%)`, transition: "all 500ms ease-in-out" }}>
-          {testImgs.map((testImg, index) => (<img src={testImg.src} key={index} width="200px" height="200px"></img>))}
+          {testImgs.map((testImg, index) => (<img alt="carousel" src={testImg.src} key={index} width="200px" height="200px"></img>))}
         </div>
       </div>
       <button onClick={() => { setCurrentIdx(currentIdx + 1); }}>앞으로</button>
